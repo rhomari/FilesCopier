1645f6367c3f7402bf2f313c6143c9b86a3b47bc
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-08-18 19:36:54 +0100

    Ajoutez des fichiers projet.

---
+++ FilesCopier/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilesCopier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> copiedfiles = BackupFiles(@"D:\source dir\", @"d:\destination dir\");
+            Console.WriteLine("{0} files copied : ",copiedfiles.Count);
+            foreach (string copiedfile in copiedfiles)
+            {
+                Console.WriteLine(copiedfile);
+            }
+            Console.Read();
+        }
+        static List<string> GetFiles(string rootdirectory,string searchpattern)
+        {
+            string[] directories = Directory.GetFiles(rootdirectory,searchpattern,SearchOption.AllDirectories);
+
+            return directories.ToList();
+        }
+        static List<string> BackupFiles(string srcpath, string dstpath)
+        {
+            
+            List<string> sourcefileslist = GetFiles(srcpath, "*");
+            List<string> copiedfileslist = new List<string>();
+            foreach (string file in sourcefileslist)
+            {
+
+                string destfile = Path.Combine(new string[] { dstpath, Path.GetDirectoryName(file).Substring(srcpath.Length), Path.GetFileName(file) });
+
+                if (!File.Exists(destfile))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(destfile));
+                        File.Copy(file, destfile, true);
+                        copiedfileslist.Add(destfile);
+                    }
+                    catch (Exception ex )
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                   
+                }
+
+            }
+            return copiedfileslist;
+        }
+    }
+}
